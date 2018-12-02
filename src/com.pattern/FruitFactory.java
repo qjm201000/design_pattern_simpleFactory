108da860827c84331c0560912bd17d7a24ca655a
qjm201000 <1529062902@qq.com>
2018-12-02 13:18:48 +0800

    简单工厂模式

--- src/com.pattern/FruitFactory.java
+++ src/com.pattern/FruitFactory.java
@@ -4,6 +4,14 @@
 public class FruitFactory {
 
     public static Fruit getFruit(String fruitName) throws ClassNotFoundException, IllegalAccessException, InstantiationException {
+        //原简单工厂模式写法
+//        if(fruitName.equalsIgnoreCase("apple")){
+//            return new Apple();
+//        }else if(fruitName.equalsIgnoreCase("banana")){
+//            return new Banana();
+//        }
+//        return null;
+        //简单工厂模式优化后的代码如下：
        Class cls =  Class.forName(fruitName);
        return (Fruit) cls.newInstance();
     }
